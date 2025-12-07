411a8512bfafd3549adff7f548da537b3bc5e990
Jovan <jsjovan94@gmail.com>
2025-12-08 00:35:47 +0100

    Closes #3: add notification service

---
+++ notification-service/src/main/java/com/kafkapoc/notificationservice/dto/OrderDto.java
@@ -0,0 +1,10 @@
+package com.kafkapoc.notificationservice.dto;
+
+import java.math.BigDecimal;
+
+public record OrderDto (
+        Integer orderId,
+        String productName,
+        Integer quantity,
+        BigDecimal price
+) {}
